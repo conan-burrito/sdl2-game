ec60d0b965cd19edd49bb47ccd871269b2ebb5af
YoWiDiN <yowidin@gmail.com>
2020-09-08 12:52:51 +0200

    Build for Android only on MacOS

--- .ci/build-android.py
+++ .ci/build-android.py
@@ -1,9 +1,5 @@
 import os
-import platform
 
 os.chdir('android')
-if platform.system() == 'Windows':
-    os.system('./gradlew assemble')
-else:
-    os.system('gradlew.bat assemble')
+os.system('./gradlew assemble')
 
